e9abfe6ef3b786dddd5384b66d1784634380bee4
JaydeeSalazar <109349276+JaydeeSalazar@users.noreply.github.com>
2024-01-04 13:53:12 +0800

    Updated Inventory System Frontend 2.2
    Tried to changed things so that the stage can be draggable. Was not successful

--- src/application/Main.java
+++ src/application/Main.java
@@ -17,6 +17,7 @@ public class Main extends Application {
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setTitle("Inventory System");
+			primaryStage.setResizable(false);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} 
