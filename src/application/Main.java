056a7349e45380413f4b3d106a919f57fa0bc364
JaydeeSalazar <109349276+JaydeeSalazar@users.noreply.github.com>
2024-01-04 10:50:49 +0800

    Testing Commit

--- src/application/Main.java
+++ src/application/Main.java
@@ -7,14 +7,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
-
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage)
 	{
 		try 
 		{
-			Parent root = FXMLLoader.load(getClass().getResource("InventorySystem.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("/InventorySystem.fxml"));
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
