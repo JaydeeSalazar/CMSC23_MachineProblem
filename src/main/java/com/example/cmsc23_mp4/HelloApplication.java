88e4f073b4c87aa88c8b25e6202a4b493564a301
JaydeeSalazar <109349276+JaydeeSalazar@users.noreply.github.com>
2024-01-11 22:59:44 +0800

    Inventory System FrontEnd 4
    Added CSS elements to the frontend

--- src/main/java/com/example/cmsc23_mp4/HelloApplication.java
+++ src/main/java/com/example/cmsc23_mp4/HelloApplication.java
@@ -7,20 +7,24 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class HelloApplication extends Application 
+{
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws IOException
+    {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        scene.getStylesheets().add(getClass().getResource("FrontEndDesign.css").toExternalForm());
         stage.setTitle("Inventory System");
-        stage.setMinWidth(1100);
-        stage.setMinHeight(620);
+        stage.setMinWidth(1137);
+        stage.setMinHeight(640);
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         launch();
     }
 }
